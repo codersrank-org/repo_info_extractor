c23cf4b9effa05d3951bcf9e1d9e67abac6f888c
Peter Karakas <karakas.peter@gmail.com>
2019-05-24 10:29:54 +0200

    Refactoring

---
+++ src/analyze_repo.py
@@ -0,0 +1,39 @@
+import os
+
+from entity.commit import Commit
+from entity.repository import Repository
+
+class AnalyzeRepo:
+    def __init__(self, repo):
+        self.repo = repo
+        self.commit_list = {}
+
+    
+    def create_commits_entity_from_branch(self, branch):
+        """
+        Extract the commits from a given branch
+        """
+        n = 100
+        commits = list(self.repo.iter_commits(branch, max_count=n)) 
+        skip = 0
+        while len(commits) > 0:
+            for commit in commits:
+                if self.commit_list.has_key(commit.hexsha):
+                    break
+                self.commit_list[commit.hexsha] = Commit(commit.author.name, commit.author.email, commit.committed_datetime, commit.hexsha, commit.parents, commit.stats.files, branch)
+                print('Get commit ' + commit.hexsha + ' from branch ' + branch)
+            skip += n
+            commits = list(self.repo.iter_commits(branch, max_count=n, skip=skip))
+
+    def create_repo_entity(self, repo_dir):
+        return Repository(os.path.basename(repo_dir), self.repo, self.commit_list)
+
+    def flag_duplicated_commits(self):
+        for hash in self.commit_list:
+            if self.commit_list[hash].is_merge:
+                count = 0
+                for parent in self.commit_list[hash].parents:
+                    if self.commit_list.has_key(parent):
+                        count += 1
+                if count > 1:
+                    self.commit_list[hash].is_duplicated = True
