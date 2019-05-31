1ec93e2a3f1aeab055a69c96da9340fbd419ad02
Peter Karakas <karakas.peter@gmail.com>
2019-05-31 11:03:18 +0200

    Zip the output file

--- src/export_result.py
+++ src/export_result.py
@@ -1,4 +1,5 @@
 import json
+from zipfile import ZipFile
 
 class ExportResult:
     def __init__(self, data):
@@ -8,3 +9,9 @@ class ExportResult:
         f = open(file_name, 'w+')
         f.write(json.dumps(self.data.json_ready(), indent=4))
         f.close()
+        # Zip the output
+        print('Zip output file...')
+        with ZipFile(file_name + '.zip','w', compression=8) as zip: 
+            zip.write(file_name)
+            zip.close()
+        print('Result has has been generated in: ' + file_name + '.zip')
