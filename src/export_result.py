7a427eed823efdf2c47b2bc43ec2a3f0219f6bc3
Shashank Agarwal <shashank.agarwal@razorpay.com>
2019-07-28 19:44:46 +0530

    Added upload functanility

--- src/export_result.py
+++ src/export_result.py
@@ -1,6 +1,10 @@
 import json
 import os
 from zipfile import ZipFile
+from ui.questions import Questions
+from upload import uploadRepo
+import webbrowser
+
 
 class ExportResult:
     def __init__(self, data):
@@ -14,5 +18,20 @@ class ExportResult:
         with ZipFile(file_name + '.zip','w', compression=8) as zip: 
             zip.write(file_name)
             zip.close()
+
+        print('Result has has been saved in: ' + file_name + '.zip')    
+        q = Questions()
+
+        result = q.query_yes_no('Would you like to upload it?')
+        if result:
+            response = uploadRepo( file_name + '.zip')
+            if response is not None:
+                reponame = os.path.splitext(file_name)[0]
+                reponame = reponame.replace(".","")
+                reponame = reponame.replace("/","")
+                url = 'https://profile.codersrank.io/user?token='+response['token']+'&reponame='+reponame
+                # print("Open this url to upload the private repo:")
+                # print(url)
+                webbrowser.open(url)
+
         os.remove(file_name)
-        print('Result has has been saved in: ' + file_name + '.zip')
---
+++ src/upload.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from ui.progress import progress
+
+def uploadRepo(filename):
+	url = "http://127.0.0.1:9900/candidate/privaterepo/Upload"
+	fin = open(filename, 'rb')
+	files = {'file': fin}
+	try:
+		print('Uploading '+filename+" ...")
+  		r = requests.post(url, files=files)
+  		json_data = json.loads(r.text)
+  		r.raise_for_status()
+		return json_data
+	except requests.exceptions.RequestException as err:
+		showError(err)		
+	except requests.exceptions.HTTPError as err:
+    		showError(err)		
+	finally:
+		fin.close()
+
+def showError(err):
+	print err
+	print '.'
+	print '.'
+	print("Unable to upload the file, please try uploading it manually.")	
+
+
+
