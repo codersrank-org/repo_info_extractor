a5e68e71ad62c36a59e914b7735ea7ad0ee039e2
Peter Karakas <karakas.peter@gmail.com>
2019-08-08 22:13:33 +0200

    Replace tabs to space

--- src/ui/questions.py
+++ src/ui/questions.py
@@ -40,33 +40,33 @@ class Questions:
         return prompt(questions)
 
     def query_yes_no(self, question, default="yes"):
-	    """Ask a yes/no question via raw_input() and return their answer.
+        """Ask a yes/no question via raw_input() and return their answer.
 
-	    "question" is a string that is presented to the user.
-	    "default" is the presumed answer if the user just hits <Enter>.
-	        It must be "yes" (the default), "no" or None (meaning
-	        an answer is required of the user).
+        "question" is a string that is presented to the user.
+        "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
 
-	    The "answer" return value is True for "yes" or False for "no".
-	    """
-	    valid = {"yes": True, "y": True, "ye": True,
-	             "no": False, "n": False}
-	    if default is None:
-	        prompt = " [y/n] "
-	    elif default == "yes":
-	        prompt = " [Y/n] "
-	    elif default == "no":
-	        prompt = " [y/N] "
-	    else:
-	        raise ValueError("invalid default answer: '%s'" % default)
+        The "answer" return value is True for "yes" or False for "no".
+        """
+        valid = {"yes": True, "y": True, "ye": True,
+                 "no": False, "n": False}
+        if default is None:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
 
-	    while True:
-	        sys.stdout.write(question + prompt)
-	        choice = raw_input().lower()
-	        if default is not None and choice == '':
-	            return valid[default]
-	        elif choice in valid:
-	            return valid[choice]
-	        else:
-	            sys.stdout.write("Please respond with 'yes' or 'no' "
-	                             "(or 'y' or 'n').\n")    
+        while True:
+            sys.stdout.write(question + prompt)
+            choice = raw_input().lower()
+            if default is not None and choice == '':
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                sys.stdout.write("Please respond with 'yes' or 'no' "
+                                 "(or 'y' or 'n').\n")    
