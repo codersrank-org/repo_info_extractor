afb7ec82629e612709e229ac09c5d4b832900907
Vlad <vlad@vivec.co.uk>
2019-09-04 15:15:29 +0300

    Provide a windows bat script to build and run docker image

--- docker.bat
+++ docker.bat
@@ -1,2 +1,3 @@
 docker build -t codersrank_extractor:latest .
-docker run -it --mount type=bind,source="%1"/,target="/repo" --mount type=bind,source="%CD%"/,target="/app" codersrank_extractor:latest
+for /F %%i in ("%1") do @set repo_name=%%~nxi
+docker run -it --mount type=bind,source="%1"/,target="/ %repo_name%" --mount type=bind,source="%CD%"/,target="/app" codersrank_extractor:latest /%repo_name%
