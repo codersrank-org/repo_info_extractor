aa41d28c910cd108b9374cd66baa12e571112db3
Vlad von Hraban <hraban@vivec.co.uk>
2019-09-13 16:19:24 +0300

    Add tests as a part of the docker build

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 .PHONY: test
 test:
 	nose2
+build-docker:
+	docker build -t codersrank/repo_info_extractor:latest .
--- test/test_JavaScript.py
+++ test/test_JavaScript.py
@@ -3,9 +3,8 @@ from language.JavaScript import extract_libraries
 
 def test_extract_libraries():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    files = ['test_fixtures/JavaScript.js']
+    files = ['fixtures/JavaScript.js']
     fq_files = [os.path.join(dir_path, f) for f in files]
     libs = extract_libraries(fq_files)
     print(libs)
     assert libs == ['lib1', 'lib2', 'lib3', 'lib4', './lib5', './lib6'], "Should extract all libraries used"
-    
