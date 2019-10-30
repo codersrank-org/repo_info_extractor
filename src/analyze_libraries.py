56976f82ef9794a079475d18ecd6e7fba32877c4
Vlad von Hraban <hraban@vivec.co.uk>
2019-10-30 18:50:14 +0300

    More verbose output regarding deleting, as well as mimic print func API in _log_info

--- src/analyze_libraries.py
+++ src/analyze_libraries.py
@@ -11,7 +11,6 @@ from language.detect_language import supported_languages
 from datetime import datetime
 
 
-
 class AnalyzeLibraries:
     def __init__(self, commit_list, author_emails, basedir):
         self.commit_list = commit_list
@@ -32,7 +31,7 @@ class AnalyzeLibraries:
         _log_info("Copying the repository to a temporary location, this can take a while...")
 
         shutil.copytree(self.basedir, tmp_repo_path, symlinks=True)
-        _log_info("Finished copying the repository")
+        _log_info("Finished copying the repository to", tmp_repo_path)
 
         # Initialise the next tmp directory as a repo and hard reset, just in case
         repo = git.Repo(tmp_repo_path)
@@ -40,7 +39,7 @@ class AnalyzeLibraries:
         try:
             repo.git.checkout('master')
         except git.exc.GitCommandError as err:
-            _log_info("Cannot checkout master on repository: %s" % err)
+            _log_info("Cannot checkout master on repository: ", err)
         repo.git.reset('--hard')
 
         prog = 0
@@ -75,22 +74,28 @@ class AnalyzeLibraries:
 
                 if libs_in_commit:
                     res[commit.hash] = libs_in_commit
-        except Exception as err:
-            print("Count not analyse libraries: %s" % err)
-            traceback.print_exc()
 
-        shutil.rmtree(tmp_repo_path)
+        except (Exception, KeyboardInterrupt) as err:
+            # make sure to clean up the tmp folder before dying
+            _cleanup(tmp_repo_path)
+            raise err
+
+        _cleanup(tmp_repo_path)
         return res
 
+def _cleanup(tmp_repo_path):
+    _log_info("Deleting", tmp_repo_path)
+    shutil.rmtree(tmp_repo_path)
 
 # Return only commits authored by provided obfuscated_author_emails
 def _filter_commits_by_author_emails(commit_list, author_emails):
-    _log_info("Filtering commits by emails: %s" % author_emails)
+    _log_info("Filtering commits by emails: ", author_emails)
     return list(filter(lambda x:  x.author_email in author_emails, commit_list))
 
 
 def _get_temp_repo_path():
     return os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
 
-def _log_info(message):
-    print("[%s] %s ." % message)	
+def _log_info(message, *argv):
+    timed_message = "[%s] %s" % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"), message)
+    print(timed_message, *argv)
