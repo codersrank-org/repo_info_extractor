42802684718400b5dae9da1f678dc949cec371a5
Zsolt Rabai <39801472+rabxly@users.noreply.github.com>
2019-12-02 20:42:35 +0100

    Cod 830 (#67)
    * test
    
    * fixed windows bug
    
    * first test
    
    * Windows bugfix last version
    
    * added jupyter notebook
    
    * removed blank lines
    
    * fixed blank lines
    
    * final commit
    
    * Added logging to extractor
    
    * changed the location of checkout
    
    * Added check for large commits
    
    * changed to a single checkout per commit
    
    * fixed _should_we_check_out
    
    * added logging for regex
    
    * commented regexes for testing purposes
    
    * fixed progress bar
    
    * re-added regexes
    
    * switched order of size est and lang check
    
    * logging creation moved to init.py
    
    * added extra args
    
    * fixed argument bugs
    
    * added extra logging
    
    * fixed arguments
    
    * fixed noskip
    
    * added skip to lang_files_filtered
    
    * changed position of message
    
    * added extra arguments for commit and file size
    
    * analyze_repo.py refactor
    
    * Possible fix for disk space leaking

--- src/analyze_libraries.py
+++ src/analyze_libraries.py
@@ -4,18 +4,26 @@ import hashlib as md5
 import tempfile
 import shutil
 import git
+import stat
 import uuid
 from ui.progress import progress
 from language.loader import load as load_language
 from language.detect_language import supported_languages
 from datetime import datetime
+import logging
+import time
+
+module_logger = logging.getLogger("main.analyze_libraries")
 
 
 class AnalyzeLibraries:
-    def __init__(self, commit_list, author_emails, basedir):
+    def __init__(self, commit_list, author_emails, basedir, skip, commit_size_limit, file_size_limit):
         self.commit_list = commit_list
         self.author_emails = author_emails
         self.basedir = basedir
+        self.skip = skip
+        self.commit_size_limit = commit_size_limit
+        self.file_size_limit = file_size_limit
 
     # Return a dict of commit -> language -> list of libraries
     def get_libraries(self):
@@ -27,7 +35,7 @@ class AnalyzeLibraries:
 
         # Before we do anything, copy the repo to a temporary location so that we don't mess with the original repo
         tmp_repo_path = _get_temp_repo_path()
-        
+
         _log_info("Copying the repository to a temporary location, this can take a while...")
 
         shutil.copytree(self.basedir, tmp_repo_path, symlinks=True)
@@ -45,24 +53,69 @@ class AnalyzeLibraries:
         prog = 0
         total = len(commits)
 
+        if not self.skip:
+            _log_info("Skipping is set to False. All commits and files will be evaluated. This may take time.")
+        else:
+            _log_info("Commit size limit is {} MBs and file size limit is {} MBs.".format(
+                self.commit_size_limit, self.file_size_limit))
+
         try:
             for commit in commits:
+                start = time.time()
+                module_logger.debug("Current commit hash is {}.".format(commit.hash))
                 libs_in_commit = {}
                 files = [os.path.join(tmp_repo_path, x.file_name)
                          for x in commit.changed_files]
+
+                # if skip is not set to false in args, we may skip certain commits
+                # Estimate the summed size of the changed files in the commit. If changed files sum more than 10 MB
+                # or there are no changed files we recognize, we skip the commit (don't check out)
+                est_size = _estimate_changed_file_size(files)
+                module_logger.debug("Changed file list is {} MBs.".format(est_size))
+                module_logger.debug("Skip is set to {}.".format(self.skip))
+                if not self.skip or ((est_size < self.commit_size_limit) and _should_we_check_out(files)):
+
+                    module_logger.debug("Checking out and analyzing commit.")
+                    co_start = time.time()
+                    try:
+                        repo.git.checkout(commit.hash, force=True)
+                    except Exception:
+                        continue
+                    co_end = time.time()
+                    module_logger.debug("Checking out took {0:.6f} seconds.".format(co_end - co_start))
+
+                else:
+                    module_logger.debug("Skipping commit.")
+                    prog += 1
+                    progress(prog, total, 'Analyzing libraries')
+                    continue
+
                 for lang, extensions in supported_languages.items():
                     # we have extensions now, filter the list to only files with those extensions
                     lang_files = list(filter(lambda x: (pathlib.Path(
                         x).suffix[1:].lower() in extensions), files))
                     if lang_files:
+                        module_logger.debug("Current language is {}, and extensions are{}".format(lang, extensions))
                         # if we go to this point, there were files modified in the language we support
                         # check out the commit in our temporary branch
-                        try:
-                            repo.git.checkout(commit.hash, force=True)
-                        except Exception:
-                            continue
+
                         # we need to filter again for files, that got deleted during the checkout
-                        lang_files_filtered = list(filter(lambda x: os.path.isfile(x), lang_files))
+                        # we also filter out tiles, which are larger than 2 MB to speed up the process
+                        if self.skip:
+                            filter_func = (lambda x: os.path.isfile(x) and os.stat(x).st_size
+                                           < self.file_size_limit * (1024**2))
+                        else:
+                            filter_func = (lambda x: os.path.isfile(x))
+
+                        lang_files_filtered = list(filter(filter_func
+                                                          , lang_files))
+
+                        total_size = sum(os.stat(f).st_size for f in lang_files_filtered)
+                        module_logger.debug("The number of files in lang_files_filtered"
+                                            " is {0}, the total size is {1:.2f} MB".
+                                            format(
+                                                 len(lang_files_filtered), total_size / (1024 ** 2)
+                                             ))
                         # now we need to run regex for imports for every single of such file
                         # Load the language plugin that is responsible for parsing those files for libraries used
                         parser = load_language(lang)
@@ -75,6 +128,10 @@ class AnalyzeLibraries:
                                 parser.extract_libraries(lang_files_filtered))
 
                 prog += 1
+                end = time.time()
+                module_logger.debug("Time spent processing commit {0} was {1:.4f} seconds.".format(
+                    commit.hash, end-start))
+
                 progress(prog, total, 'Analyzing libraries')
 
                 if libs_in_commit:
@@ -88,21 +145,50 @@ class AnalyzeLibraries:
         _cleanup(tmp_repo_path)
         return res
 
+
+def _should_we_check_out(file_list):
+
+    for lang, extensions in supported_languages.items():
+        lang_files = list(filter(lambda x: (pathlib.Path(x).suffix[1:].lower() in extensions), file_list))
+        if lang_files:
+            return True
+    return False
+
+
+def _estimate_changed_file_size(file_list):
+    total_size = 0
+    for file in file_list:
+        try:
+            total_size += os.stat(file).st_size / (1024**2)
+        except FileNotFoundError:
+            continue
+    return total_size
+
+
+def _remove_readonly(func, path, _):
+
+    """Clear the readonly bit and reattempt the removal"""
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
 def _cleanup(tmp_repo_path):
     _log_info("Deleting", tmp_repo_path)
     try:
-        shutil.rmtree(tmp_repo_path)
+        shutil.rmtree(tmp_repo_path, onerror=_remove_readonly)
     except (PermissionError, NotADirectoryError) as e:
         _log_info("Error when deleting {}".format(str(e)))
+
 
 # Return only commits authored by provided obfuscated_author_emails
 def _filter_commits_by_author_emails(commit_list, author_emails):
     _log_info("Filtering commits by emails: ", author_emails)
-    return list(filter(lambda x:  x.author_email in author_emails, commit_list))
+    return list(filter(lambda x: x.author_email in author_emails, commit_list))
 
 
 def _get_temp_repo_path():
     return os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+
 
 def _log_info(message, *argv):
     timed_message = "[%s] %s" % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"), message)
--- src/language/JavaScript.py
+++ src/language/JavaScript.py
@@ -1,8 +1,13 @@
 import re
+import logging
+import time
+import os
 
 """
 Extract a list of JS libraries used from a fully qualified paths of files
 """
+
+js_logger = logging.getLogger("main.analyze_libraries.javascript")
 
 
 def extract_libraries(files):
@@ -13,12 +18,22 @@ def extract_libraries(files):
     # ES6 imports
     regex2 = re.compile(r'import\s*(?:.+ from)?\s?\(?[\'"](.+)[\'"]\)?;?\s', re.IGNORECASE)
     for f in files:
+        size = os.stat(f).st_size / (1024**2)
+        js_logger.debug("Opening file {0}. The size of the file is {1:.4f} MB.".format(f, size))
+        start1 = time.time()
         with open(file=f, mode='r', errors='ignore') as fr:
-
-            contents = ' '.join(fr.readlines())
+            end1 = time.time()
+            lines = fr.readlines()
+            js_logger.debug("# of line in {} is {}.".format(f, len(lines)))
+            start2 = time.time()
+            contents = ' '.join(lines)
             matches = regex1.findall(contents)
             matches.extend(regex2.findall(contents))
 
+            end2 = time.time()
+            js_logger.debug("Time spent on open for {0} is {1:.6f} seconds.".format(f, end1 - start1))
+            js_logger.debug("Time spent processing {0} is {1:.6f} seconds.".format(f, end2 - start2))
         if matches:
+            js_logger.debug("Library found in {}. The first 20 chars of matches is {}".format(f, matches[0][0:20]))
             res.extend(matches)
     return res
