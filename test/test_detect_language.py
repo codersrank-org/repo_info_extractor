7fdc05322b4ecb7db42d240c73d9b62af289a3a8
Viktor Lazarev <taurus101v@gmail.com>
2019-12-26 09:32:08 +0100

    Detect Elm lang

--- test/test_detect_language.py
+++ test/test_detect_language.py
@@ -32,6 +32,7 @@ def test_languages_recognised():
     assert detect_language.detect_language("/tmp/some_file.gvy") == "Groovy"
     assert detect_language.detect_language("/tmp/some_file.ex") == "Elixir"
     assert detect_language.detect_language("/tmp/some_file.exs") == "Elixir"
+    assert detect_language.detect_language("/tmp/some_file.elm") == "Elm"
     assert detect_language.detect_language("/tmp/some_file.erb") == "ERB"
     assert detect_language.detect_language("/tmp/some_file.html.erb") == "ERB"
     assert detect_language.detect_language("/tmp/some_file.fs") == "F#"
