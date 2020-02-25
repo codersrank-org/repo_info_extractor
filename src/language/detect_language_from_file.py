ddd587ac95ec179b370effa346873126d4705c07
Alim Giray Aytar <alimgiray@Alims-MacBook-Pro.local>
2020-02-25 18:09:15 +0300

    use Pygments instead of binary files

--- src/language/detect_language_from_file.py
+++ src/language/detect_language_from_file.py
@@ -2,23 +2,18 @@ import os
 import sys
 import subprocess
 import json
+from pygments.lexers import guess_lexer
 
 def detect_language_from_file(file_path, repo_dir):
-
-    enry_path = '/app/src/bin/enry_linux'
-
-    # This is for mac os beacuse we need to use different binaries for different platforms
-    if sys.platform == "darwin":
-        enry_path = os.getcwd() + '/src/bin/enry'
-
     full_path = repo_dir + "/" + file_path
     if os.path.exists(full_path):
         try:
-            output = subprocess.run([enry_path, "--json", full_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-            if output.stderr is None:
-                output = json.loads(output.stdout.strip())
-                return output["language"]
-            else:
-                return None    
+            with open(full_path, 'r') as file:
+                code = file.read()
+                language = guess_lexer(code)
+                if language.name == 'Objective-C':
+                    return language.name
+                else:
+                    return "MATLAB"
         except:
             return None
