25b0515d3a3a745991d77baeac3a5f53773f8636
rabxly <rabxly@gmail.com>
2020-11-02 11:33:26 +0100

    fixed test error

--- test/test_Swift.py
+++ test/test_Swift.py
@@ -8,21 +8,20 @@ def test_extract_libraries():
     fq_files = [os.path.join(dir_path, f) for f in files]
     libs = extract_libraries(fq_files)
     assert len(libs) == 1
+    assert len(libs['Swift']) == 15
 
-    assert libs['Swift'] == [
-        'Cocoa',
-        'os.log',
-        'StatsKit',
-        'ModuleKit',
-        'CPU',
-        'Memory',
-        'Disk',
-        'Net',
-        'Battery',
-        'Sensors',
-        'GPU',
-        'Fans',
-        'CoreServices.DictionaryServices',
-        'Pentathlon.swim',
-        'test.test'
-        ]
+    assert 'Cocoa' in libs['Swift']
+    assert 'os.log' in libs['Swift']
+    assert 'StatsKit' in libs['Swift']
+    assert 'ModuleKit' in libs['Swift']
+    assert 'CPU' in libs['Swift']
+    assert 'Disk' in libs['Swift']
+    assert 'Memory' in libs['Swift']
+    assert 'Net' in libs['Swift']
+    assert 'Battery' in libs['Swift']
+    assert 'Sensors' in libs['Swift']
+    assert 'GPU' in libs['Swift']
+    assert 'Fans' in libs['Swift']
+    assert 'CoreServices.DictionaryServices' in libs['Swift']
+    assert 'Pentathlon.swim' in libs['Swift']
+    assert 'test.test' in libs['Swift']
