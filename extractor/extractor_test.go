c7f1e4a520d9672d0cba9fa3f5a38e740a549253
Peter Karakas <karakas.peter@gmail.com>
2021-03-12 10:56:29 +0100

    Add correct repo_name to the artifact
    The repo_name had .git at the end. Like this: codersrank-org/repo_info_extractor.git

---
+++ extractor/extractor_test.go
@@ -0,0 +1,33 @@
+package extractor_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/codersrank-org/repo_info_extractor/extractor"
+)
+
+var _ = Describe("Extractor", func() {
+
+	Context("RepoExtractor headless", func() {
+		It("should the repo name with the owner name", func() {
+			re := extractor.RepoExtractor{
+				Headless: true,
+			}
+			Expect(re.GetRepoName("git@github.com:alimgiray/repo_info_extractor.git")).To(Equal("alimgiray/repo_info_extractor"))
+			Expect(re.GetRepoName("https://github.com/alimgiray/repo_info_extractor.git")).To(Equal("alimgiray/repo_info_extractor"))
+		})
+	})
+
+	Context("RepoExtractor interactive", func() {
+		It("should the repo name without the owner name", func() {
+			re := extractor.RepoExtractor{
+				Headless: false,
+				RepoPath: "/some/path/alimgiray/repo_info_extractor",
+			}
+			Expect(re.GetRepoName("git@github.com:alimgiray/repo_info_extractor.git")).To(Equal("repo_info_extractor"))
+			Expect(re.GetRepoName("https://github.com/alimgiray/repo_info_extractor.git")).To(Equal("repo_info_extractor"))
+			Expect(re.GetRepoName("")).To(Equal("repo_info_extractor"))
+		})
+	})
+})
