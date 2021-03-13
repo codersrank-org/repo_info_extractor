76c949339086de8353b351e93c29f738cab3cda3
Alim Giray Aytar <alimgiray@gmail.com>
2021-03-13 11:23:24 +0300

    add python library detection

---
+++ librarydetection/languages/Python.go
@@ -0,0 +1,24 @@
+package languages
+
+import (
+	"regexp"
+
+	"github.com/codersrank-org/repo_info_extractor/librarydetection"
+)
+
+// NewPythonScriptAnalyzer constructor
+func NewPythonScriptAnalyzer() librarydetection.Analyzer {
+	return &pythonScriptAnalyzer{}
+}
+
+type pythonScriptAnalyzer struct{}
+
+func (a *pythonScriptAnalyzer) ExtractLibraries(contents string) []string {
+	fromRegex, err := regexp.Compile(`from (.+) import`)
+	importRegex, err := regexp.Compile(`import ([a-zA-Z0-9_-]+)(?:\s| as)`)
+	if err != nil {
+		panic(err)
+	}
+
+	return executeRegexes(contents, []*regexp.Regexp{fromRegex, importRegex})
+}
