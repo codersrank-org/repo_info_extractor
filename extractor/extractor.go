aaa84f65f63b5a0bbd85031b5165c82949a27350
Alim Giray Aytar <alimgiray@gmail.com>
2021-03-13 12:15:44 +0300

    add Java library detection

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -176,6 +176,7 @@ func (r *RepoExtractor) initAnalyzers() {
 	librarydetection.AddAnalyzer("Go", languages.NewGoAnalyzer())
 	librarydetection.AddAnalyzer("C", languages.NewCAnalyzer())
 	librarydetection.AddAnalyzer("C#", languages.NewCSharpAnalyzer())
+	librarydetection.AddAnalyzer("Java", languages.NewJavaAnalyzer())
 	librarydetection.AddAnalyzer("JavaScript", languages.NewJavaScriptAnalyzer())
 	librarydetection.AddAnalyzer("TypeScript", languages.NewTypeScriptAnalyzer())
 	librarydetection.AddAnalyzer("Python", languages.NewPythonScriptAnalyzer())
