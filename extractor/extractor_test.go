557d30a7775d22f0cb3dd135997b539789355ce5
Peter Karakas <karakas.peter@gmail.com>
2021-04-10 17:58:22 +0200

    Improve tests

--- extractor/extractor_test.go
+++ extractor/extractor_test.go
@@ -7,21 +7,22 @@ import (
 	"github.com/codersrank-org/repo_info_extractor/extractor"
 )
 
-var _ = Describe("Extractor", func() {
+var _ = Describe("GetRepoName", func() {
 
 	Context("RepoExtractor headless", func() {
-		It("should the repo name with the owner name", func() {
+		It("should get the repo name with the owner name", func() {
 			re := extractor.RepoExtractor{
 				Headless: true,
 			}
 			Expect(re.GetRepoName("git@github.com:alimgiray/repo_info_extractor.git")).To(Equal("alimgiray/repo_info_extractor"))
 			Expect(re.GetRepoName("https://github.com/alimgiray/repo_info_extractor.git")).To(Equal("alimgiray/repo_info_extractor"))
 			Expect(re.GetRepoName("https://github.com/peti2001-test/second-project.git")).To(Equal("peti2001-test/second-project"))
+			Expect(re.GetRepoName("ssh://user@host:port/group/repoName.git")).To(Equal("group/repoName"))
 		})
 	})
 
 	Context("RepoExtractor interactive", func() {
-		It("should the repo name without the owner name", func() {
+		It("should get the repo name without the owner name", func() {
 			re := extractor.RepoExtractor{
 				Headless: false,
 				RepoPath: "/some/path/alimgiray/repo_info_extractor",
@@ -29,6 +30,7 @@ var _ = Describe("Extractor", func() {
 			Expect(re.GetRepoName("git@github.com:alimgiray/repo_info_extractor.git")).To(Equal("repo_info_extractor"))
 			Expect(re.GetRepoName("https://github.com/alimgiray/repo_info_extractor.git")).To(Equal("repo_info_extractor"))
 			Expect(re.GetRepoName("")).To(Equal("repo_info_extractor"))
+			Expect(re.GetRepoName("ssh://user@host:port/group/repoName.git")).To(Equal("repoName"))
 		})
 	})
 })
