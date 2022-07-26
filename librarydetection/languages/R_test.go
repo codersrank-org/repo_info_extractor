8230a5335982c368e9f66dd496af33f14a8abd30
kitswas <90329875+kitswas@users.noreply.github.com>
2022-07-26 20:18:00 +0530

    Added tests for R

---
+++ librarydetection/languages/R_test.go
@@ -0,0 +1,41 @@
+package languages_test
+
+import (
+	"io/ioutil"
+
+	. "github.com/onsi/ginkgo"
+
+	"github.com/codersrank-org/repo_info_extractor/v2/librarydetection/languages"
+)
+
+var _ = Describe("RLibraryDetection", func() {
+	fixture, err := ioutil.ReadFile("./fixtures/r.fixture")
+	if err != nil {
+		panic(err)
+	}
+
+	expectedLibraries := []string{
+		"ggplot",
+		"quantmod",
+		"tibble",
+		"tidyr",
+		"shiny",
+		"bslib",
+		"DT",
+		"WGCNA",
+		"impute",
+		"BiocManager",
+	}
+
+	analyzer := languages.NewRAnalyzer()
+
+	Describe("Extract R Libraries", func() {
+		It("Should be able to extract libraries", func() {
+			libs, err := analyzer.ExtractLibraries(string(fixture))
+			if err != nil {
+				panic(err)
+			}
+			assertSameUnordered(libs, expectedLibraries)
+		})
+	})
+})
